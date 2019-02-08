16f846b550339f50f853e2cf062b0c2110303789
死の金属と数学。 <35822767+satanmyninjas@users.noreply.github.com>
2019-02-07 20:50:43 -0500

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,2 +1,13 @@
 def gallows():
-  pass
+  head = " 0 "
+  arm_2_spaces = "/  "
+  arm_torso_space = "/| "
+  arm_torso_arm = "/|\ "
+  fused_legs = " ^ "
+  extended_legs = "/ \ "
+
+  extremities = [head, arm_2_spaces, arm_torso_space, arm_torso_arm, fused_legs, extended_legs]
+  
+  while True:
+    for _ in extremities:
+      yield _
