1b3b59c403a5784532a3e79257c4d77444f9a6b0
死の金属と数学。 <>
2019-02-21 20:35:06 -0500

    more addition to while statement, add counter for correct guesses, add TODO

--- hangman.py
+++ hangman.py
@@ -33,27 +33,32 @@ def hang():
 
 
 # randomly selecting a word
+# TODO: fix this hot mess
 def choose_word():
     with open('words.csv', mode='r') as csv_file:
         yield random.choice(list(csv_file)).lower()  # avoids proper nouns by lower casing everything
 
 
-# while statement where game is actually ran.
 # other functions are defined above to be used down here for reference
 mistakes = 0  # once 8 repetitions of hang() is done, game over
-secret_word = choose_word()
+secret_word = list(choose_word())
 underscores = "_ " * len(secret_word)
-tries_left = int((8 - {mistakes}))
+tries_left = int((8 - (mistakes)))
 lynch = hang()
+win_count = 0  # will go up if you guess correctly, if win_count == len(secret_word), you win
+
+# while statement where game is actually ran.
+# other functions are defined above to be used down here for reference
 while mistakes < 8:
     f'HANGMAN \n'
-    f'{underscores}'
+    print(underscores)
     guess = input("Enter a letter: ")
     if guess == secret_word:
         for letter in range(len(secret_word)):
             if guess == secret_word[letter]:
                 secret_word[letter] = secret_word[letter]
         f'Correct! Keep going. '
+        win_count += 1
         # replace the blank with what the user guessed
     elif guess != secret_word:
         mistakes += 1
@@ -61,33 +66,36 @@ while mistakes < 8:
         f'Incorrect. So far, you have {tries_left} remaining chances. '
     elif mistakes == 8:
         f'Game over!\n'
+    elif win_count == len(secret_word):
+        f'Congratulations, you won!'
+        play_again = input("Would you like to stop playing? y / n \n").lower()
+        if play_again == "n":
+            break
 
+# TODO: guess a letter
+# TODO: if guess is incorrect, call hang() and  mistakes += 1
+# TODO: if guess is correct, continue on
 
-    # guess a letter
-    # if guess is incorrect, call hang() and  mistakes += 1
-    # if guess is correct, continue on
+# TODO: keep checking if the guess matches the word
+# TODO: once mistakes = 8, game over
 
-    # keep checking if the guess matches the word
-    # once mistakes = 8, game over
+# TODO: give option to play again, y / n
+# TODO: if yes, go back to prompting for word length / difficulty for new word
+# TODO: if no, break
 
-    # give option to play again, y / n
-    # if yes, go back to prompting for word length / difficulty for new word
-    # if no, break
+# look up csv docs
 
-    # look up csv docs
+# TODO: for every letter, **try to replace w/ underscores
+# TODO: guess
+# TODO: if guess is incorrect, call hang() and  mistakes += 1
+# TODO: if guess is correct, continue on
 
-    # for every letter, **try to replace w/ underscores
-    # guess
-    # if guess is incorrect, call hang() and  mistakes += 1
-    # if guess is correct, continue on
+# TODO: keep checking if the guess matches the word
+# TODO: once mistakes = 8, game over
 
-    # keep checking if the guess matches the word
-    # once mistakes = 8, game over
+# TODO: give option to play again, y / n
+# TODO: if yes, go back to prompting for word length / difficulty for new word
+# TODO: if no, break
 
-    # give option to play again, y / n
-    # if yes, go back to prompting for word length / difficulty for new word
-    # if no, break
-
-    # add a default mode for **any word in the file if player doesn't specify a length
-    # look up csv docs
+# (optional) add a default mode for **any word in the file if player doesn't specify a length
 
