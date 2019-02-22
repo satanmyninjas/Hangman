87dff25c70d84465e8ebef064e011ec1e171b764
死の金属と数学。 <>
2019-02-21 19:05:15 -0500

    current progress: flesh out **some of while loop,

--- hangman.py
+++ hangman.py
@@ -35,24 +35,34 @@ def hang():
 # randomly selecting a word
 def choose_word():
     with open('words.csv', mode='r') as csv_file:
-        yield (random.choice(list(csv_file))).lower()
+        yield random.choice(list(csv_file)).lower()  # avoids proper nouns by lower casing everything
 
 
 # while statement where game is actually ran.
 # other functions are defined above to be used down here for reference
-mistakes = 0  # once eight repetitions of hang() is done, game over
+mistakes = 0  # once 8 repetitions of hang() is done, game over
 secret_word = choose_word()
+underscores = "_ " * len(secret_word)
+tries_left = int((8 - {mistakes}))
+lynch = hang()
 while mistakes < 8:
+    f'HANGMAN \n'
+    f'{underscores}'
     guess = input("Enter a letter: ")
     if guess == secret_word:
-        f'Correct!'
+        for letter in range(len(secret_word)):
+            if guess == secret_word[letter]:
+                secret_word[letter] = secret_word[letter]
+        f'Correct! Keep going. '
         # replace the blank with what the user guessed
-    else:
-        pass
+    elif guess != secret_word:
+        mistakes += 1
+        print(next(lynch))
+        f'Incorrect. So far, you have {tries_left} remaining chances. '
+    elif mistakes == 8:
+        f'Game over!\n'
 
-    # implement difficulty or word length (at least try)
-    # pick word at random
-    # for every letter, **try to replace w/ underscores
+
     # guess a letter
     # if guess is incorrect, call hang() and  mistakes += 1
     # if guess is correct, continue on
@@ -64,7 +74,6 @@ while mistakes < 8:
     # if yes, go back to prompting for word length / difficulty for new word
     # if no, break
 
-    # add a default mode for **any word in the file if player doesn't specify a length
     # look up csv docs
 
     # for every letter, **try to replace w/ underscores
